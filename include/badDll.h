6268fdb97e68fa2f3b55b0d863af13a0c9d0cdac
m3ndax <adrian.goessl@outlook.com>
2026-01-31 14:13:56 +0000

    fixed declspec for windows....

--- include/badDll.h
+++ include/badDll.h
@@ -4,7 +4,20 @@
 #if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+#ifdef _WIN32
+#  define DLL_EXPORT __declspec(dllexport)
+#else
+#  define DLL_EXPORT
+#endif
+
+DLL_EXPORT BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved);
+
+#ifdef __cplusplus
+}
+#endif
 #endif
 #endif
